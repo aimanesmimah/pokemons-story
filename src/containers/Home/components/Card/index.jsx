fe2dff848a3d71336681cd334d30f966a68f0628
cloudstrife9494 <cloudistrife.94@gmail.com>
2020-10-16 09:13:13 +0100

    card functional comp

--- src/containers/Home/components/Card/index.jsx
+++ src/containers/Home/components/Card/index.jsx
@@ -1,44 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import AppRoutes from '../../../../constants/app-routes.constants';
 import './styles.scss';
-import { connect } from 'react-redux';
-import { setPokemonId } from '../../../../store/pokemon/actions';
-import { Link } from 'react-router-dom';
 
-class Card extends React.PureComponent {
+function Card({  name, height, weight, base_experience, picture, id }) {
 
-    handleCardClick= e => {
-      const { id, setPokemonId } = this.props;
-      setPokemonId(id);
-      this.setState({ toViewPokemon: true });
-    }
-
-    render() {
-      const {
-        name, height, weight, base_experience, picture, id
-      } = this.props;
-
-      return (
-          <Link className="pokemon--card" to={AppRoutes.VIEW_POKEMON_ID.replace(':id', id)} >
-              <img alt="" className="pokemon--card__logo" src={picture}
-                    /* onLoad={handleImageLoaded} onError={handleLoadError} */ />
-              <div className="pokemon--card__content" >
-                  <div>{name}</div>
-                  <div className="pokemon--card__content--infos" >
-                      <p>Height, <br/><span>{height}</span></p>
-                      <p>Weight, <br/><span>{weight}</span></p>
-                      <p>Exp, <br/><span>{base_experience}</span></p>
-                  </div>
-              </div>
-          </Link>);
-    }
+    return (
+        <Link className="pokemon--card" to={AppRoutes.VIEW_POKEMON_ID.replace(':id', id)} >
+            <img alt="" className="pokemon--card__logo" src={picture} />
+            <div className="pokemon--card__content" >
+                <div>{name}</div>
+                <div className="pokemon--card__content--infos" >
+                    <p>Height, <br/><span>{height}</span></p>
+                    <p>Weight, <br/><span>{weight}</span></p>
+                    <p>Exp, <br/><span>{base_experience}</span></p>
+                </div>
+            </div>
+        </Link>);
 }
 
-export default connect(
-  null,
-  dispatch => ({
-    setPokemonId(id) {
-      dispatch(setPokemonId(id));
-    }
-  })
-)(Card);
+export default Card
